050bc21bcd40a50de570af0c106617f8b56cb4ce
jsonmona <jsonmona@outlook.com>
2026-03-24 17:26:08 +0900

    모든 이미지 처리하는 코드

--- llm2doc/analyze_layout.py
+++ llm2doc/analyze_layout.py
@@ -1,28 +1,36 @@
 import numpy as np
+import os
 
 from PIL import Image, ImageDraw
 
+from paddleocr import LayoutDetection
 
-def analyze_layout(img: Image.Image):
-    from paddleocr import LayoutDetection
+# Use PP-DocLayoutV2 to get reading order
+model = LayoutDetection(
+    model_name="PP-DocLayoutV2",
+    layout_nms=True,
+    threshold=0.5,
+    layout_merge_bboxes_mode="large",
+)
+
+def analyze_layout(img: Image.Image, save_path: str):
 
     img_arr = np.asarray(img.convert("RGB"))[..., ::-1].copy()
-
-    # Use PP-DocLayoutV2 to get reading order
-    model = LayoutDetection(
-        model_name="PP-DocLayout_plus-L",
-        layout_nms=True,
-        threshold=0.5,
-        layout_merge_bboxes_mode="large",
-    )
 
     output = model.predict(img_arr)
 
     for res in output:
-        res.print()
-        res.save_to_img(save_path="./debug")
+        res.save_to_img(save_path=save_path)
+
+
+def main():
+    os.makedirs("paddle", exist_ok=True)
+
+    for file_name in os.listdir("images"):
+        print(f"Processing {file_name}...", flush=True)
+        img = Image.open(f"images/{file_name}")
+        analyze_layout(img, f"./paddle/{file_name}")
 
 
 if __name__ == "__main__":
-    img = Image.open("data/financial/original.png")
-    analyze_layout(img)
+    main()
