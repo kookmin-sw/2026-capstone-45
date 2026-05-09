fad0f60a38dff344d7d3de2d4897c75ca6f040bd
jsonmona <jsonmona@outlook.com>
2026-05-10 00:26:45 +0900

    GitHub Pages cayman 테마 적용

---
+++ docs/preview.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+docker run --rm -it \
+  --volume="$PWD:/srv/jekyll" \
+  --publish 5002:4000 \
+  -e PAGES_REPO_NWO=local/preview \
+  jekyll/jekyll:latest \
+  jekyll serve --watch --force_polling
