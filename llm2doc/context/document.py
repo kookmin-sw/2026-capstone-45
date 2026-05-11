5b849a10b40de1ce677cd37e12a065714fe42aae
jsonmona <jsonmona@outlook.com>
2026-05-11 18:02:31 +0900

    문서 생성시 로그기능 추가

--- llm2doc/context/document.py
+++ llm2doc/context/document.py
@@ -1,9 +1,14 @@
+import asyncio
+import logging
+
 from typing import Mapping, Sequence, TypeVar, Type, cast
 from dataclasses import dataclass
 from pydantic import BaseModel
 from PIL import Image
 
 from llm2doc.artifact.base import ArtifactPipeline
+from llm2doc.context.pipeline import PipelineContext
+from llm2doc.repository.document import append_document_log
 from llm2doc.util import validate_type
 
 
@@ -12,6 +17,7 @@ T = TypeVar("T", bound=BaseModel)
 
 @dataclass(frozen=True)
 class DocumentContext:
+    pipeline_ctx: PipelineContext
     doc_id: int
     images: Sequence[Image.Image]
     artifacts: Mapping[str, BaseModel]
@@ -24,3 +30,11 @@ class DocumentContext:
 
         validate_type(value, pipeline.ARTIFACT)
         return cast(T, value)
+
+    async def append_log(self, message: str):
+        logging.info(message)
+        async with self.pipeline_ctx.with_db() as db:
+            await append_document_log(db, self.doc_id, message)
+
+    def append_log_sync(self, message: str):
+        asyncio.run_coroutine_threadsafe(self.append_log(message), loop=self.pipeline_ctx.loop)
